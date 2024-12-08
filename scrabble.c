3527865c771c4521c2bb3d9f67e352229aa14b3e
AOgit <AOgit@users.noreply.github.com>
2024-12-08 13:00:01 +0100

    automated commit by check50 [check50=True]

--- scrabble.c
+++ scrabble.c
@@ -1,14 +1,16 @@
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
 
 #define SIZE = 26;
+char chars[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+int points[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
+int get_points(string str);
 
-int get_points (string str);
-
-int main (void)
+int main(void)
 {
     string answer1 = get_string("Player 1: ");
     string answer2 = get_string("Player 2: ");
@@ -16,6 +18,7 @@ int main (void)
     int result1 = get_points(answer1);
     int result2 = get_points(answer2);
 
+    // Compare received results
     if (result1 > result2)
     {
         printf("Player 1 wins!\n");
@@ -28,15 +31,11 @@ int main (void)
     {
         printf("Tie!\n");
     }
-
-
-
 }
 
-int get_points (string str)
+int get_points(string str)
 {
-    char chars[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-    int points[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+
     int summ = 0;
 
     // Take each char of the string
@@ -45,9 +44,9 @@ int get_points (string str)
         // Iterate over the array of chars
         for (int j = 0; j < 26; j++)
         {
-            //summ +=  (str[i] == chars[j]) ? points[j] : 0;
-            // compare char of string and char in array
-            if  (toupper(str[i]) == chars[j])
+            // summ +=  (str[i] == chars[j]) ? points[j] : 0;
+            // Compare char of string and char in array
+            if (toupper(str[i]) == chars[j])
             {
                 summ += points[j];
                 break;
@@ -56,5 +55,3 @@ int get_points (string str)
     }
     return summ;
 }
-
-
