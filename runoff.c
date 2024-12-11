47e2e33dbd971df07285c94e8cc0a0981d6ed524
AOgit <AOgit@users.noreply.github.com>
2024-12-11 22:05:02 +0100

    automated commit by submit50

--- runoff.c
+++ runoff.c
@@ -129,7 +129,7 @@ bool vote(int voter, int rank, string name)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        if  (strcmp(name, candidates[i].name) == 0)
+        if (strcmp(name, candidates[i].name) == 0)
         {
             preferences[voter][rank] = i;
             return true;
@@ -149,8 +149,8 @@ void tabulate(void)
             candidat = preferences[i][j];
             if (!candidates[candidat].eliminated)
             {
-                    candidates[candidat].votes++;
-                    break;
+                candidates[candidat].votes++;
+                break;
             }
         }
     }
