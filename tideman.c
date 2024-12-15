24d0745bc5b259c23033e8bcc4d3627335bf1d7d
AOgit <AOgit@users.noreply.github.com>
2024-12-15 14:30:33 +0100

    automated commit by check50 [check50=True]

--- tideman.c
+++ tideman.c
@@ -187,13 +187,13 @@ void lock_pairs(void)
     for (int i = 0; i < pair_count; i++)
     {
 
-       if (lockered[pairs[i].loser] == true)
+       if (lockered[pairs[i].loser] == false)
        {
             lockered[pairs[i].loser] = true;
             counter_lockered++;
        }
 
-    if (counter_lockered < candidate_count)
+    if (counter_lockered <= candidate_count)
     {
         locked[pairs[i].winner][pairs[i].loser] = true;
     }
