a8fe6b189c48e79d4b2d2555c9723c884d0865b4
AOgit <AOgit@users.noreply.github.com>
2024-12-19 18:26:11 +0100

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -50,10 +50,10 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     // Loop over all pixels
     for (int i = 0; i < height; i++)
     {
-        for (int j = 0; j < width; j++)
+        for (int j = 0; j < width/2; j++)
         {
-             if (j == width/2 )
-                break;
+            // if (j == width/2 )
+           //     break;
             // Swap pixels
             sepia_ij = image[i][j];
             image[i][j] = image[i][width - j];
