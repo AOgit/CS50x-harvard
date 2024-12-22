50b092b3c738935c1fdfa487fe0b7d60a29c6196
AOgit <AOgit@users.noreply.github.com>
2024-12-22 19:54:51 +0100

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
 int main(int argc, char *argv[])
 {
     if (argc != 2)
@@ -15,19 +17,38 @@ int main(int argc, char *argv[])
     uint8_t buffer[512];
 
     int i = 0;
+    char filename[8];
+    sprintf(filename, "%03i.jpg", i);
+    FILE *outputfile = fopen(filename, "w");
 
     while (fread(buffer, 1, 512, card) == 512)
     {
+
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff  && ((buffer[3] & 0xf0) == 0xe0)  )
         {
-            char filename[8];
-            sprintf(filename, "%03i.jpg", i);
-            FILE *outputfile = fopen(filename, "w");
-            fwrite(buffer, 1, 512, outputfile);
-            // printf("%s foto writed\n", filensame);
-            fclose(outputfile);
+
+            if (i == 0)
+            {
+                fwrite(buffer, 1, 512, outputfile);
+            }
+            else
+            {
+                fclose(outputfile);
+                sprintf(filename, "%03i.jpg", i);
+                outputfile = fopen(filename, "w");
+                fwrite(buffer, 1, 512, outputfile);
+            }
             i++;
         }
+         else
+        {
+            if (i > 0)
+            {
+                fwrite(buffer, 1, 512, outputfile);
+            }
+        }
+
     }
+   // fclose(outputfile);
     fclose(card);
 }
