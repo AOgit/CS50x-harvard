de99571d0f5ba69ac8db03198a092bae7bcc96b5
AOgit <AOgit@users.noreply.github.com>
2024-12-22 19:58:48 +0100

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -2,8 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-
 int main(int argc, char *argv[])
 {
     if (argc != 2)
@@ -24,7 +22,8 @@ int main(int argc, char *argv[])
     while (fread(buffer, 1, 512, card) == 512)
     {
 
-        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff  && ((buffer[3] & 0xf0) == 0xe0)  )
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff &&
+            ((buffer[3] & 0xf0) == 0xe0))
         {
 
             if (i == 0)
@@ -40,14 +39,13 @@ int main(int argc, char *argv[])
             }
             i++;
         }
-         else
+        else
         {
             if (i > 0)
             {
                 fwrite(buffer, 1, 512, outputfile);
             }
         }
-
     }
     fclose(outputfile);
     fclose(card);
