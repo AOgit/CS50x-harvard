a1bc34c7bf1cc6ec9cc155b5a29cb0c04b7527e7
AOgit <AOgit@users.noreply.github.com>
2025-01-03 00:27:43 +0100

    automated commit by check50 [check50=True]

---
+++ 12.sql
@@ -0,0 +1,18 @@
+SELECT DISTINCT(title)
+FROM movies
+WHERE id IN
+(
+    SELECT DISTINCT(movies.id)
+    FROM movies
+    JOIN stars ON stars.movie_id = movies.id
+    JOIN people ON people.id = stars.person_id
+    WHERE people.name = "Jennifer Lawrence"
+) AND id IN
+(
+    SELECT DISTINCT(movies.id)
+    FROM movies
+    JOIN stars ON stars.movie_id = movies.id
+    JOIN people ON people.id = stars.person_id
+    WHERE people.name = "Bradley Cooper"
+);
+
